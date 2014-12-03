ce7f6d35d142fcae0b8e3947364a661b10e38f92
xww <xiongwenwu1997@126.com>
2014-12-03 09:33:03 +0800

    version 1.1

---
+++ core/src/main/java/org/storevm/toolkits/session/catalina/CatalinaDistributedSessionFacade.java
@@ -0,0 +1,25 @@
+/**
+ * Storevm.org Inc.
+ * Copyright (c) 2004-2010 All Rights Reserved.
+ */
+package org.storevm.toolkits.session.catalina;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.session.StandardSessionFacade;
+
+/**
+ * Session门面，兼容Tomcat的处理方法
+ * @author xiangqing.tan
+ * @version $Id: CatalinaDistributedSession.java, v 0.1 2010-12-31 下午05:16:01 xiangqing.tan Exp $
+ */
+public class CatalinaDistributedSessionFacade extends StandardSessionFacade {
+    /**
+     * 构造方法
+     * @param session
+     */
+    public CatalinaDistributedSessionFacade(HttpSession session) {
+        super(session);
+    }
+
+}
